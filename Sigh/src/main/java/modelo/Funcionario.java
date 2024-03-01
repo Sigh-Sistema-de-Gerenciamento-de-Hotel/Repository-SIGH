1e888cc0cad7a2064af3b126381ffad762aad6f4
Aluno <lara.p2006@aluno.ifsc.edu.br>
2024-03-01 18:27:02 -0300

    Funcionario Select

--- Sigh/src/main/java/modelo/Funcionario.java
+++ Sigh/src/main/java/modelo/Funcionario.java
@@ -6,6 +6,36 @@ public class Funcionario {
 	private String usuario;
 	private String senha;
 	private String cargo;
-	private Setor setor; 
+	private Setor setor;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public Setor getSetor() {
+		return setor;
+	}
+	public void setSetor(Setor setor) {
+		this.setor = setor;
+	} 
 	
 }
