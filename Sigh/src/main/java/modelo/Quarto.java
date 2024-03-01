56ce8771c7b62085a0e575b1838b58e4d455b6f6
Aluno <lara.p2006@aluno.ifsc.edu.br>
2024-03-01 19:46:53 -0300

    Select Quarto

--- Sigh/src/main/java/modelo/Quarto.java
+++ Sigh/src/main/java/modelo/Quarto.java
@@ -5,7 +5,7 @@ public class Quarto {
 	private int numero;
 	private int numCamaCasal;
 	private int numCamaSolteiro;
-	private int nimMaxHospedes;
+	private int numMaxHospedes;
 	private boolean arCondicionado;
 	private boolean frigobar;
 	private boolean banheira;
@@ -13,4 +13,71 @@ public class Quarto {
 	private float preco;
 	private boolean precisaLimpeza;
 	private boolean precisaConserto;
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public int getNumCamaCasal() {
+		return numCamaCasal;
+	}
+	public void setNumCamaCasal(int numCamaCasal) {
+		this.numCamaCasal = numCamaCasal;
+	}
+	public int getNumCamaSolteiro() {
+		return numCamaSolteiro;
+	}
+	public void setNumCamaSolteiro(int numCamaSolteiro) {
+		this.numCamaSolteiro = numCamaSolteiro;
+	}
+	public int getNumMaxHospedes() {
+		return numMaxHospedes;
+	}
+	public void setNumMaxHospedes(int numMaxHospedes) {
+		this.numMaxHospedes = numMaxHospedes;
+	}
+	public boolean isArCondicionado() {
+		return arCondicionado;
+	}
+	public void setArCondicionado(boolean arCondicionado) {
+		this.arCondicionado = arCondicionado;
+	}
+	public boolean isFrigobar() {
+		return frigobar;
+	}
+	public void setFrigobar(boolean frigobar) {
+		this.frigobar = frigobar;
+	}
+	public boolean isBanheira() {
+		return banheira;
+	}
+	public void setBanheira(boolean banheira) {
+		this.banheira = banheira;
+	}
+	public boolean isAcessibilidade() {
+		return acessibilidade;
+	}
+	public void setAcessibilidade(boolean acessibilidade) {
+		this.acessibilidade = acessibilidade;
+	}
+	public float getPreco() {
+		return preco;
+	}
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+	public boolean isPrecisaLimpeza() {
+		return precisaLimpeza;
+	}
+	public void setPrecisaLimpeza(boolean precisaLimpeza) {
+		this.precisaLimpeza = precisaLimpeza;
+	}
+	public boolean isPrecisaConserto() {
+		return precisaConserto;
+	}
+	public void setPrecisaConserto(boolean precisaConserto) {
+		this.precisaConserto = precisaConserto;
+	}
+	
 }
