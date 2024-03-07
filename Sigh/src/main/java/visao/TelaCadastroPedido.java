3d69933e2429119dda6f8cc3e9a5d440b09a2fea
Gabrieli <gabrieli.rb@aluno.ifsc.edu.br>
2024-03-07 11:41:22 -0300

    Pedido tela

--- Sigh/src/main/java/visao/TelaCadastroPedido.java
+++ Sigh/src/main/java/visao/TelaCadastroPedido.java
@@ -14,37 +14,30 @@ import javax.swing.ImageIcon;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.SwingConstants;
-import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.Font;
 
 public class TelaCadastroPedido extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField txtNome;
+	private JLabel lblNome;
 	private JLabel lblDepartamento;
 	private JLabel lblBotaoSalvar;
 	private JLabel lblBotaoCancelar;
 	private JTextField txtDescricao;
+	private JTextField txtNumReserva;
 	private JLabel lblQuarto;
+	private JLabel lblNumReserva;
 	private JLabel lblDescricao;
+	private JTextField txtPreco;
+	private JLabel lblHospedag;
 	private JComboBox comboBox2Departamento;
 	private JComboBox comboBox1Quarto;
-	private JLabel lblCadastroPedido;
-	private JLabel lblPedidosPequeno;
-	private JLabel lblHospedes;
-	private JLabel lblTextHospedagem;
-	private JLabel lblTextFuncionarios;
-	private JLabel lblBotaoSair;
-	private JLabel lblConta;
-	private JLabel lblEmail;
-	private JLabel lblNomeConta;
-	private JLabel lblNome;
-	private JTextField txtNome;
-	private JTextField txtNumeroReserva;
 	private JTextField txtData;
-	private JTextField txtHorario;
-	private JTextField txtHospedagem;
+	private JTextField txtHora;
 	private JTextField txtFeito;
 
 	/**
@@ -77,225 +70,266 @@ public class TelaCadastroPedido extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		lblEmail = new JLabel("Julia.Almeida@sigh.com");
-		lblEmail.setBounds(39, 598, 181, 28);
-		contentPane.add(lblEmail);
-		
-		lblNomeConta = new JLabel("JULIA ALMEIDA");
-		lblNomeConta.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblNomeConta.setBounds(39, 585, 90, 14);
-		contentPane.add(lblNomeConta);
-		
-		lblConta = new JLabel("Conta");
-		lblConta.setBounds(29, 552, 46, 14);
-		contentPane.add(lblConta);
-		
-		lblTextFuncionarios = new JLabel("Funcionários");
-		lblTextFuncionarios.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblTextFuncionarios.setBounds(72, 501, 78, 14);
-		contentPane.add(lblTextFuncionarios);
-		
-		lblTextHospedagem = new JLabel("Hospedagem");
-		lblTextHospedagem.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblTextHospedagem.setBounds(72, 435, 78, 14);
-		contentPane.add(lblTextHospedagem);
-		
-		lblHospedes = new JLabel("Hóspedes");
-		lblHospedes.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblHospedes.setBounds(72, 375, 70, 14);
-		contentPane.add(lblHospedes);
-		
-		JLabel lblTextPedidos = new JLabel("Pedidos");
-		lblTextPedidos.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblTextPedidos.setBounds(72, 317, 46, 14);
-		contentPane.add(lblTextPedidos);
-		
-		lblBotaoSair = new JLabel("");
-		lblBotaoSair.addMouseListener(new MouseAdapter() {
+		JLabel lblSair = new JLabel("");
+		lblSair.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				setVisible(false);
+				setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			}
 			@Override
 			public void mouseEntered(MouseEvent e) {
+				lblSair.setIcon(new ImageIcon("src/main/resources/botao sair cinza escuro.png"));
 			}
 			@Override
 			public void mouseExited(MouseEvent e) {
+				lblSair.setIcon(new ImageIcon("src/main/resources/botao sair.png"));
 			}
 		});
-		lblBotaoSair.setIcon(new ImageIcon("src/main/resources/botao sair.png"));
-		lblBotaoSair.setBounds(10, 645, 277, 69);
-		contentPane.add(lblBotaoSair);
 		
-		JLabel lblIconeFunionarios = new JLabel("");
-		lblIconeFunionarios.setIcon(new ImageIcon("src/main/resources/icone funcionarios (menu).png"));
-		lblIconeFunionarios.setBounds(39, 489, 24, 26);
-		contentPane.add(lblIconeFunionarios);
+		JLabel lblCadastrarPedidoGrande = new JLabel("CADASTRAR PEDIDO");
+		lblCadastrarPedidoGrande.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblCadastrarPedidoGrande.setBounds(401, 79, 224, 26);
+		contentPane.add(lblCadastrarPedidoGrande);
 		
-		JLabel lblIconeHospedagem = new JLabel("");
-		lblIconeHospedagem.setIcon(new ImageIcon("src/main/resources/icone Hospedagem (menu).png"));
-		lblIconeHospedagem.setBounds(41, 424, 24, 36);
-		contentPane.add(lblIconeHospedagem);
+		JLabel lblCadastrarPedido = new JLabel("Cadastrar Pedido");
+		lblCadastrarPedido.setBounds(431, 11, 126, 14);
+		contentPane.add(lblCadastrarPedido);
 		
-		JLabel lblIocneHospedes = new JLabel("");
-		lblIocneHospedes.setIcon(new ImageIcon("src/main/resources/icone hospedes (menu).png"));
-		lblIocneHospedes.setBounds(39, 363, 24, 26);
-		contentPane.add(lblIocneHospedes);
+		JLabel lblPedidosNaBarra = new JLabel("Pedidos >");
+		lblPedidosNaBarra.setBounds(370, 11, 60, 14);
+		contentPane.add(lblPedidosNaBarra);
 		
-		JLabel lblIconePedidos = new JLabel("");
-		lblIconePedidos.setIcon(new ImageIcon("src/main/resources/icone pedidos (menu).png"));
-		lblIconePedidos.setBounds(41, 311, 34, 20);
-		contentPane.add(lblIconePedidos);
+		JLabel lblFuncionarios = new JLabel("");
+		lblFuncionarios.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				lblFuncionarios.setIcon(new ImageIcon("src/main/resources/menu - funcionarios selecionado.png"));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				lblFuncionarios.setIcon(new ImageIcon("src/main/resources/menu funcionarios.png"));
+			}
+		});
+		lblFuncionarios.setIcon(new ImageIcon("src/main/resources/menu funcionarios.png"));
+		lblFuncionarios.setBounds(10, 465, 339, 49);
+		contentPane.add(lblFuncionarios);
+		
+		JLabel lblHospedagem = new JLabel("");
+		lblHospedagem.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				lblHospedagem.setIcon(new ImageIcon("src/main/resources/menu hospedagem selecionado.png"));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				lblHospedagem.setIcon(new ImageIcon("src/main/resources/menu hospedagem.png"));
+			}
+		});
+		lblHospedagem.setIcon(new ImageIcon("src/main/resources/menu hospedagem.png"));
+		lblHospedagem.setBounds(10, 405, 339, 49);
+		contentPane.add(lblHospedagem);
+		
+		JLabel lblHospedes = new JLabel("");
+		lblHospedes.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				lblHospedes.setIcon(new ImageIcon("src/main/resources/menu - hospedes selecionado.png"));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				lblHospedes.setIcon(new ImageIcon("src/main/resources/menu - hospede.png"));
+			}
+		});
+		lblHospedes.setIcon(new ImageIcon("src/main/resources/menu - hospede.png"));
+		lblHospedes.setBounds(10, 345, 339, 49);
+		contentPane.add(lblHospedes);
+		
+		JLabel lblPedidos = new JLabel("");
+		lblPedidos.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				lblPedidos.setIcon(new ImageIcon("src/main/resources/menu - pedidos selecionado.png"));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				lblPedidos.setIcon(new ImageIcon("src/main/resources/menu pedidos.png"));
+			}
+		});
+		lblPedidos.setIcon(new ImageIcon("src/main/resources/menu pedidos.pngg"));
+		lblPedidos.setBounds(10, 284, 339, 50);
+		contentPane.add(lblPedidos);
+		
+		JLabel lblNomeUsuario = new JLabel("JULIA ALMEIDA");
+		lblNomeUsuario.setBounds(51, 585, 92, 14);
+		contentPane.add(lblNomeUsuario);
+		
+		JLabel lblConta = new JLabel("Conta");
+		lblConta.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblConta.setBounds(32, 560, 46, 14);
+		contentPane.add(lblConta);
 		
 		JLabel lblMenu = new JLabel("Menu");
-		lblMenu.setBounds(29, 274, 46, 14);
+		lblMenu.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblMenu.setBounds(33, 259, 46, 14);
 		contentPane.add(lblMenu);
-		
-		JLabel lblGrandeCadastrar = new JLabel("CADASTRAR PEDIDO");
-		lblGrandeCadastrar.setFont(new Font("Tahoma", Font.PLAIN, 28));
-		lblGrandeCadastrar.setBounds(313, 73, 365, 44);
-		contentPane.add(lblGrandeCadastrar);
-		
-		lblCadastroPedido = new JLabel("Cadastrar Pedido");
-		lblCadastroPedido.setBounds(356, 11, 114, 14);
-		contentPane.add(lblCadastroPedido);
-		
-		lblPedidosPequeno = new JLabel("Pedidos >");
-		lblPedidosPequeno.setBounds(297, 11, 58, 14);
-		contentPane.add(lblPedidosPequeno);
+		lblSair.setIcon(new ImageIcon("src/main/resources/botao sair.png"));
+		lblSair.setBounds(43, 629, 268, 55);
+		contentPane.add(lblSair);
 		
 		JLabel lblLogoSIGH = new JLabel("");
 		lblLogoSIGH.setIcon(new ImageIcon("src/main/resources/logo sigh.png"));
-		lblLogoSIGH.setBounds(61, 11, 149, 237);
+		lblLogoSIGH.setBounds(108, 11, 149, 237);
 		contentPane.add(lblLogoSIGH);
 		
 		JLabel lblTelaCinza = new JLabel("");
 		lblTelaCinza.setIcon(new ImageIcon("src/main/resources/fundo cinza (menu).png"));
-		lblTelaCinza.setBounds(0, 0, 287, 749);
+		lblTelaCinza.setBounds(0, 0, 355, 749);
 		contentPane.add(lblTelaCinza);
 		
 		JLabel lblTelaBrancaMenor = new JLabel("");
 		lblTelaBrancaMenor.setIcon(new ImageIcon("src/main/resources/Hero block.png"));
-		lblTelaBrancaMenor.setBounds(285, 0, 1085, 36);
+		lblTelaBrancaMenor.setBounds(351, 0, 1019, 36);
 		contentPane.add(lblTelaBrancaMenor);
 		
 		JLabel lblTelaBrancaMaior = new JLabel("");
 		lblTelaBrancaMaior.setIcon(new ImageIcon("src/main/resources/Hero block.png"));
-		lblTelaBrancaMaior.setBounds(285, 61, 1085, 69);
+		lblTelaBrancaMaior.setBounds(351, 61, 1019, 69);
 		contentPane.add(lblTelaBrancaMaior);
 		
 		JLabel label = new JLabel("New label");
 		label.setBounds(51, 57, 46, 14);
 		contentPane.add(label);
 		
+		txtNome = new RoundJFormattedTextField(null);
+		txtNome.setBounds(418, 222, 183, 26);
+		contentPane.add(txtNome);
+		txtNome.setColumns(10);
+		
+		lblNome = new JLabel("Nome*");
+		lblNome.setBounds(418, 206, 139, 14);
+		contentPane.add(lblNome);
+		
 		lblDepartamento = new JLabel("Departamento*");
-		lblDepartamento.setBounds(1024, 286, 92, 14);
+		lblDepartamento.setBounds(1013, 286, 92, 14);
 		contentPane.add(lblDepartamento);
 		
 		lblBotaoSalvar = new JLabel("");
 		lblBotaoSalvar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				
+				//PROGRAMAR BOTAO DE SALVAR
 			}
 			@Override
 			public void mouseEntered(MouseEvent e) {
+				lblBotaoSalvar.setIcon(new ImageIcon("src/main/resources/botao salvar verde claro.png"));
 			}
 			@Override
 			public void mouseExited(MouseEvent e) {
+				lblBotaoSalvar.setIcon(new ImageIcon("src/main/resources/botao salvar.png"));
 			}
 		});
 		lblBotaoSalvar.setIcon(new ImageIcon("src/main/resources/botao salvar.png"));
-		lblBotaoSalvar.setBounds(824, 624, 292, 77);
+		lblBotaoSalvar.setBounds(787, 611, 292, 77);
 		contentPane.add(lblBotaoSalvar);
 		
 		lblBotaoCancelar = new JLabel("");
 		lblBotaoCancelar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				setVisible(false);
+			}
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				lblBotaoCancelar.setIcon(new ImageIcon("src/main/resources/botao cancelar azul escuro.png"));
+			}	
+			@Override
+			public void mouseExited(MouseEvent e) {
+				lblBotaoCancelar.setIcon(new ImageIcon("src/main/resources/botao cancelar.png"));
 			}
 		});
 		lblBotaoCancelar.setIcon(new ImageIcon("src/main/resources/botao cancelar.png"));
-		lblBotaoCancelar.setBounds(1170, 645, 139, 44);
+		lblBotaoCancelar.setBounds(1133, 627, 139, 44);
 		contentPane.add(lblBotaoCancelar);
 		
 		txtDescricao = new RoundJFormattedTextField(null);
 		txtDescricao.setHorizontalAlignment(SwingConstants.LEFT);
-		txtDescricao.setBounds(459, 475, 443, 85);
+		txtDescricao.setBounds(418, 455, 486, 85);
 		contentPane.add(txtDescricao);
 		txtDescricao.setColumns(10);
 		
+		txtNumReserva = new RoundJFormattedTextField(null);
+		txtNumReserva.setBounds(418, 300, 183, 26);
+		contentPane.add(txtNumReserva);
+		txtNumReserva.setColumns(10);
+		
 		lblQuarto = new JLabel("Quarto*");
-		lblQuarto.setBounds(1024, 201, 55, 14);
+		lblQuarto.setBounds(1013, 206, 46, 14);
 		contentPane.add(lblQuarto);
 		
+		lblNumReserva = new JLabel("Número da Reserva*");
+		lblNumReserva.setBounds(418, 286, 120, 14);
+		contentPane.add(lblNumReserva);
+		
 		lblDescricao = new JLabel("Descrição*");
-		lblDescricao.setBounds(459, 450, 92, 14);
+		lblDescricao.setBounds(418, 441, 92, 14);
 		contentPane.add(lblDescricao);
 		
+		txtPreco = new RoundJFormattedTextField(null);
+		txtPreco.setBounds(418, 383, 183, 26);
+		contentPane.add(txtPreco);
+		txtPreco.setColumns(10);
+		
+		lblHospedag = new JLabel("Hospedagem*");
+		lblHospedag.setBounds(418, 369, 92, 14);
+		contentPane.add(lblHospedag);
+		
 		comboBox2Departamento = new JComboBox();
-		comboBox2Departamento.setBounds(1024, 311, 164, 20);
+		comboBox2Departamento.setBounds(1013, 300, 183, 26);
 		contentPane.add(comboBox2Departamento);
 		
 		comboBox1Quarto = new JComboBox();
-		comboBox1Quarto.setBounds(1024, 221, 164, 20);
+		comboBox1Quarto.setBounds(1013, 222, 177, 26);
 		contentPane.add(comboBox1Quarto);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(85, 311, 46, 14);
-		contentPane.add(lblNewLabel);
-		
-		lblNome = new JLabel("Nome*");
-		lblNome.setBounds(458, 201, 93, 14);
-		contentPane.add(lblNome);
-		
-		txtNome = new RoundJFormattedTextField(null);
-		txtNome.setBounds(458, 221, 165, 20);
-		contentPane.add(txtNome);
-		txtNome.setColumns(10);
-		
-		JLabel lblHospedagem = new JLabel("Hospedagem*");
-		lblHospedagem.setBounds(458, 375, 93, 14);
-		contentPane.add(lblHospedagem);
-		
-		txtNumeroReserva = new RoundJFormattedTextField(null);
-		txtNumeroReserva.setBounds(458, 311, 165, 20);
-		contentPane.add(txtNumeroReserva);
-		txtNumeroReserva.setColumns(10);
-		
-		JLabel lblData = new JLabel("Data*");
-		lblData.setBounds(739, 201, 46, 14);
-		contentPane.add(lblData);
-		
 		txtData = new RoundJFormattedTextField(null);
-		txtData.setBounds(738, 221, 164, 20);
+		txtData.setBounds(715, 222, 183, 26);
 		contentPane.add(txtData);
 		txtData.setColumns(10);
 		
-		JLabel lblHorario = new JLabel("Horário*");
-		lblHorario.setBounds(739, 286, 78, 14);
-		contentPane.add(lblHorario);
-		
-		txtHorario = new RoundJFormattedTextField(null);
-		txtHorario.setBounds(739, 311, 163, 20);
-		contentPane.add(txtHorario);
-		txtHorario.setColumns(10);
-		
-		JLabel lblNumeroReserva = new JLabel("Número da Reserva*");
-		lblNumeroReserva.setBounds(458, 286, 149, 14);
-		contentPane.add(lblNumeroReserva);
-		
-		txtHospedagem = new RoundJFormattedTextField(null);
-		txtHospedagem.setBounds(458, 392, 165, 20);
-		contentPane.add(txtHospedagem);
-		txtHospedagem.setColumns(10);
-		
-		JLabel lblFeito = new JLabel("Feito*");
-		lblFeito.setBounds(739, 375, 46, 14);
-		contentPane.add(lblFeito);
+		txtHora = new RoundJFormattedTextField(null);
+		txtHora.setBounds(715, 300, 183, 26);
+		contentPane.add(txtHora);
+		txtHora.setColumns(10);
 		
 		txtFeito = new RoundJFormattedTextField(null);
-		txtFeito.setBounds(739, 392, 163, 20);
+		txtFeito.setBounds(715, 383, 183, 26);
 		contentPane.add(txtFeito);
 		txtFeito.setColumns(10);
+		
+		JLabel lblData = new JLabel("Data*");
+		lblData.setBounds(715, 206, 46, 14);
+		contentPane.add(lblData);
+		
+		JLabel lblHora = new JLabel("Hora*");
+		lblHora.setBounds(715, 286, 46, 14);
+		contentPane.add(lblHora);
+		
+		JLabel lblFeito = new JLabel("Feito*");
+		lblFeito.setBounds(715, 369, 46, 14);
+		contentPane.add(lblFeito);
 	}
 }
