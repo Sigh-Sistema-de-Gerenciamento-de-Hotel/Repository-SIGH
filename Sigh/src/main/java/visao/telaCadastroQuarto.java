cdca6836a2d117e01bdaf10e96cd585a4ce296d4
Gabriely Dias <gabriely.d@aluno.ifsc.edu.br>
2024-03-28 13:29:43 -0300

    telaCadatroQuarto

---
+++ Sigh/src/main/java/visao/telaCadastroQuarto.java
@@ -0,0 +1,48 @@
+package visao;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class telaCadastroQuarto extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					telaCadastroQuarto frame = new telaCadastroQuarto();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public telaCadastroQuarto() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 944, 802);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(441, 10, 46, 14);
+		contentPane.add(lblNewLabel);
+	}
+
+}
