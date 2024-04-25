2eb4dae43a787ada73b097dddd9b3e9cf1ad3f77
Gabriely Dias <gabriely.d@aluno.ifsc.edu.br>
2024-04-25 08:56:19 -0300

    ajustes Cadastro de quarto

--- Sigh/src/main/java/visao/CadastroQuarto.java
+++ Sigh/src/main/java/visao/CadastroQuarto.java
@@ -17,37 +17,36 @@ public class CadastroQuarto extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
-	private JTextField textField_7;
-	private JTextField textField_8;
-	private JTextField textField_9;
-	private JTextField textField_10;
-	private JLabel lblNewLabel_3;
-	private JLabel lblNewLabel_4;
-	private JLabel lblNewLabel_5;
-	private JLabel lblNewLabel_6;
-	private JLabel lblNewLabel_7;
-	private JLabel lblNewLabel_8;
-	private JLabel lblNewLabel_9;
-	private JLabel lblNewLabel_10;
-	private JLabel lblNewLabel_11;
-	private JLabel lblNewLabel_12;
-	private JLabel lblNewLabel_13;
-	private JLabel lblNewLabel_14;
-	private JLabel lblNewLabel_15;
-	private JLabel lblNewLabel_16;
-	private JLabel lblNewLabel_17;
-	private JLabel lblNewLabel_18;
-	private JLabel lblNewLabel_19;
-	private JLabel lblNewLabel_20;
-	private JLabel lblNewLabel_21;
-	private JLabel lblNewLabel_22;
+	private JTextField txtNumeroQuarto;
+	private JTextField txtCamaCasal;
+	private JTextField txtCamaSolteiro;
+	private JTextField txtNumeroHospede;
+	private JTextField txtAr;
+	private JTextField txtFrigobar;
+	private JTextField txtBanheira;
+	private JTextField txtAcessibilidade;
+	private JTextField TxtLimpeza;
+	private JTextField txtConserto;
+	private JTextField txtpreco;
+	private JLabel BotaoCancelar;
+	private JLabel botaoSalvar;
+	private JLabel lblNumeroQuarto;
+	private JLabel lblCamaCasal;
+	private JLabel lblNumeroHospedes;
+	private JLabel lblAr;
+	private JLabel lblFrigobar;
+	private JLabel lblBanheira;
+	private JLabel lblAcessibilidade;
+	private JLabel lblLimpeza;
+	private JLabel lblConserto;
+	private JLabel lblPreco;
+	private JLabel lblLogo;
+	private JLabel lblMenuPedidos;
+	private JLabel lblMenuHospede;
+	private JLabel lblMenuHospedagem;
+	private JLabel lblMenuFuncionarios;
+	private JLabel lblSair;
+	private JLabel lblDivisormenu;
 
 	/**
 	 * Launch the application.
@@ -80,175 +79,175 @@ public class CadastroQuarto extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		lblNewLabel_22 = new JLabel("");
-		lblNewLabel_22.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Repository-SIGH\\Sigh\\src\\main\\resources\\divisor (menu).png"));
-		lblNewLabel_22.setBounds(90, 897, 400, 30);
-		contentPane.add(lblNewLabel_22);
+		lblDivisormenu = new JLabel("");
+		lblDivisormenu.setIcon(new ImageIcon("src/main/resources/divisor (menu).png"));
+		lblDivisormenu.setBounds(90, 897, 400, 30);
+		contentPane.add(lblDivisormenu);
 		
-		lblNewLabel_21 = new JLabel("");
-		lblNewLabel_21.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Repository-SIGH\\Sigh\\src\\main\\resources\\botao sair.png"));
-		lblNewLabel_21.setBounds(84, 955, 263, 45);
-		contentPane.add(lblNewLabel_21);
+		lblSair = new JLabel("");
+		lblSair.setIcon(new ImageIcon("src/main/resources/botao sair.png"));
+		lblSair.setBounds(84, 955, 263, 45);
+		contentPane.add(lblSair);
 		
-		lblNewLabel_20 = new JLabel("");
-		lblNewLabel_20.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Repository-SIGH\\Sigh\\src\\main\\resources\\menu funcionarios.png"));
-		lblNewLabel_20.setBounds(91, 532, 145, 22);
-		contentPane.add(lblNewLabel_20);
+		lblMenuFuncionarios = new JLabel("");
+		lblMenuFuncionarios.setIcon(new ImageIcon("src/main/resources/menu funcionarios.png"));
+		lblMenuFuncionarios.setBounds(91, 532, 145, 22);
+		contentPane.add(lblMenuFuncionarios);
 		
-		lblNewLabel_19 = new JLabel("");
-		lblNewLabel_19.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Repository-SIGH\\Sigh\\src\\main\\resources\\menu hospedagem.png"));
-		lblNewLabel_19.setBounds(91, 468, 130, 22);
-		contentPane.add(lblNewLabel_19);
+		lblMenuHospedagem = new JLabel("");
+		lblMenuHospedagem.setIcon(new ImageIcon("src/main/resources/menu hospedagem.png"));
+		lblMenuHospedagem.setBounds(91, 468, 130, 22);
+		contentPane.add(lblMenuHospedagem);
 		
-		lblNewLabel_18 = new JLabel("");
-		lblNewLabel_18.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Repository-SIGH\\Sigh\\src\\main\\resources\\menu - hospede.png"));
-		lblNewLabel_18.setBounds(92, 404, 120, 22);
-		contentPane.add(lblNewLabel_18);
+		lblMenuHospede = new JLabel("");
+		lblMenuHospede.setIcon(new ImageIcon("src/main/resources/menu - hospede.png"));
+		lblMenuHospede.setBounds(92, 404, 120, 22);
+		contentPane.add(lblMenuHospede);
 		
-		lblNewLabel_17 = new JLabel("");
-		lblNewLabel_17.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Repository-SIGH\\Sigh\\src\\main\\resources\\menu pedidos.png"));
-		lblNewLabel_17.setBounds(92, 345, 120, 22);
-		contentPane.add(lblNewLabel_17);
+		lblMenuPedidos = new JLabel("");
+		lblMenuPedidos.setIcon(new ImageIcon("src/main/resources/menu pedidos.png"));
+		lblMenuPedidos.setBounds(92, 345, 120, 22);
+		contentPane.add(lblMenuPedidos);
 		
-		lblNewLabel_16 = new JLabel("");
-		lblNewLabel_16.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Repository-SIGH\\Sigh\\src\\main\\resources\\logo sigh.png"));
-		lblNewLabel_16.setBounds(135, 42, 144, 176);
-		contentPane.add(lblNewLabel_16);
+		lblLogo = new JLabel("");
+		lblLogo.setIcon(new ImageIcon("src/main/resources/logo sigh.png"));
+		lblLogo.setBounds(135, 42, 144, 176);
+		contentPane.add(lblLogo);
 		
-		JLabel lblNewLabel_1 = new JLabel("");
-		lblNewLabel_1.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Repository-SIGH\\Sigh\\src\\main\\resources\\Cadastrar Quartos.png"));
-		lblNewLabel_1.setBounds(446, 108, 1455, 126);
-		contentPane.add(lblNewLabel_1);
+		JLabel lblCadastrarQuartos = new JLabel("");
+		lblCadastrarQuartos.setIcon(new ImageIcon("src/main/resources/Cadastrar Quartos.png"));
+		lblCadastrarQuartos.setBounds(446, 108, 1455, 126);
+		contentPane.add(lblCadastrarQuartos);
 		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Repository-SIGH\\Sigh\\src\\main\\resources\\fundo cinza (menu).png"));
-		lblNewLabel.setBounds(0, 0, 420, 1080);
-		contentPane.add(lblNewLabel);
-		
-		JLabel lblNewLabel_2 = new JLabel("");
-		lblNewLabel_2.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Repository-SIGH\\Sigh\\src\\main\\resources\\superior pequeno quartos.png"));
-		lblNewLabel_2.setBounds(408, 0, 1512, 62);
-		contentPane.add(lblNewLabel_2);
-		
-		textField =  new RoundJFormattedTextField(null);
-		textField.setBounds(491, 313, 343, 48);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		txtNumeroQuarto =  new RoundJFormattedTextField(null);
+		txtNumeroQuarto.setBounds(491, 313, 343, 48);
+		contentPane.add(txtNumeroQuarto);
+		txtNumeroQuarto.setColumns(10);
 	
 		
-		textField_1 =  new RoundJFormattedTextField(null);
-		textField_1.setBounds(491, 442, 343, 48);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		txtCamaCasal =  new RoundJFormattedTextField(null);
+		txtCamaCasal.setBounds(491, 442, 343, 48);
+		contentPane.add(txtCamaCasal);
+		txtCamaCasal.setColumns(10);
 		
-		textField_2 =  new RoundJFormattedTextField(null);
-		textField_2.setBounds(491, 579, 343, 48);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
+		txtCamaSolteiro =  new RoundJFormattedTextField(null);
+		txtCamaSolteiro.setBounds(491, 579, 343, 48);
+		contentPane.add(txtCamaSolteiro);
+		txtCamaSolteiro.setColumns(10);
 		
-		textField_3 =  new RoundJFormattedTextField(null);
-		textField_3.setColumns(10);
-		textField_3.setBounds(491, 705, 343, 48);
-		contentPane.add(textField_3);
+		txtNumeroHospede =  new RoundJFormattedTextField(null);
+		txtNumeroHospede.setColumns(10);
+		txtNumeroHospede.setBounds(491, 705, 343, 48);
+		contentPane.add(txtNumeroHospede);
 		
-		textField_4 =  new RoundJFormattedTextField(null);
-		textField_4.setColumns(10);
-		textField_4.setBounds(1017, 313, 343, 48);
-		contentPane.add(textField_4);
+		txtAr =  new RoundJFormattedTextField(null);
+		txtAr.setColumns(10);
+		txtAr.setBounds(1017, 313, 343, 48);
+		contentPane.add(txtAr);
 		
-		textField_5 =  new RoundJFormattedTextField(null);
-		textField_5.setColumns(10);
-		textField_5.setBounds(1017, 442, 343, 48);
-		contentPane.add(textField_5);
+		txtFrigobar =  new RoundJFormattedTextField(null);
+		txtFrigobar.setColumns(10);
+		txtFrigobar.setBounds(1017, 442, 343, 48);
+		contentPane.add(txtFrigobar);
 		
-		textField_6 =  new RoundJFormattedTextField(null);
-		textField_6.setColumns(10);
-		textField_6.setBounds(1017, 579, 343, 48);
-		contentPane.add(textField_6);
+		txtBanheira =  new RoundJFormattedTextField(null);
+		txtBanheira.setColumns(10);
+		txtBanheira.setBounds(1017, 579, 343, 48);
+		contentPane.add(txtBanheira);
 		
-		textField_7 = new RoundJFormattedTextField(null);
-		textField_7.setColumns(10);
-		textField_7.setBounds(1017, 705, 343, 48);
-		contentPane.add(textField_7);
+		txtAcessibilidade = new RoundJFormattedTextField(null);
+		txtAcessibilidade.setColumns(10);
+		txtAcessibilidade.setBounds(1017, 705, 343, 48);
+		contentPane.add(txtAcessibilidade);
 		
-		textField_8 =  new RoundJFormattedTextField(null);
-		textField_8.setColumns(10);
-		textField_8.setBounds(1506, 313, 343, 48);
-		contentPane.add(textField_8);
+		TxtLimpeza =  new RoundJFormattedTextField(null);
+		TxtLimpeza.setColumns(10);
+		TxtLimpeza.setBounds(1506, 313, 343, 48);
+		contentPane.add(TxtLimpeza);
 		
-		textField_9 =  new RoundJFormattedTextField(null);
-		textField_9.setColumns(10);
-		textField_9.setBounds(1506, 442, 343, 48);
-		contentPane.add(textField_9);
+		txtConserto =  new RoundJFormattedTextField(null);
+		txtConserto.setColumns(10);
+		txtConserto.setBounds(1506, 442, 343, 48);
+		contentPane.add(txtConserto);
 		
-		lblNewLabel_3 = new JLabel("");
-		lblNewLabel_3.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Repository-SIGH\\Sigh\\src\\main\\resources\\botao cancelar.png"));
-		lblNewLabel_3.setBounds(1682, 931, 140, 40);
-		contentPane.add(lblNewLabel_3);
+		BotaoCancelar = new JLabel("");
+		BotaoCancelar.setIcon(new ImageIcon("src/main/resources/botao cancelar.png"));
+		BotaoCancelar.setBounds(1682, 931, 140, 40);
+		contentPane.add(BotaoCancelar);
 		
-		textField_10 = new RoundJFormattedTextField(null);
-		textField_10.setColumns(10);
-		textField_10.setBounds(1506, 579, 343, 48);
-		contentPane.add(textField_10);
+		txtpreco = new RoundJFormattedTextField(null);
+		txtpreco.setColumns(10);
+		txtpreco.setBounds(1506, 579, 343, 48);
+		contentPane.add(txtpreco);
 		
-		lblNewLabel_4 = new JLabel("");
-		lblNewLabel_4.setIcon(new ImageIcon("C:\\Users\\Aluno\\Desktop\\Repository-SIGH\\Sigh\\src\\main\\resources\\botao salvar.png"));
-		lblNewLabel_4.setBounds(1310, 928, 292, 54);
-		contentPane.add(lblNewLabel_4);
+		botaoSalvar = new JLabel("");
+		botaoSalvar.setIcon(new ImageIcon("src/main/resources/botao salvar.png"));
+		botaoSalvar.setBounds(1310, 928, 292, 54);
+		contentPane.add(botaoSalvar);
 		
-		lblNewLabel_5 = new JLabel("Número do Quarto ");
-		lblNewLabel_5.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_5.setBounds(491, 289, 120, 20);
-		contentPane.add(lblNewLabel_5);
+		lblNumeroQuarto = new JLabel("Número do Quarto ");
+		lblNumeroQuarto.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNumeroQuarto.setBounds(491, 289, 120, 20);
+		contentPane.add(lblNumeroQuarto);
 		
-		lblNewLabel_6 = new JLabel("Cama Casal");
-		lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_6.setBounds(491, 411, 120, 20);
-		contentPane.add(lblNewLabel_6);
+		lblCamaCasal = new JLabel("Cama Casal");
+		lblCamaCasal.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblCamaCasal.setBounds(491, 411, 120, 20);
+		contentPane.add(lblCamaCasal);
 		
-		lblNewLabel_7 = new JLabel("Cama Solteiro");
-		lblNewLabel_7.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_7.setBounds(491, 548, 120, 20);
-		contentPane.add(lblNewLabel_7);
+		JLabel lblCamaSolteiro = new JLabel("Cama Solteiro");
+		lblCamaSolteiro.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblCamaSolteiro.setBounds(491, 548, 120, 20);
+		contentPane.add(lblCamaSolteiro);
 		
-		lblNewLabel_8 = new JLabel("Número de óspedes");
-		lblNewLabel_8.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_8.setBounds(491, 674, 150, 20);
-		contentPane.add(lblNewLabel_8);
+		lblNumeroHospedes = new JLabel("Número de hóspedes");
+		lblNumeroHospedes.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNumeroHospedes.setBounds(491, 674, 150, 20);
+		contentPane.add(lblNumeroHospedes);
 		
-		lblNewLabel_9 = new JLabel("Ar-Condicionado");
-		lblNewLabel_9.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_9.setBounds(1017, 282, 120, 20);
-		contentPane.add(lblNewLabel_9);
+		lblAr = new JLabel("Ar-Condicionado");
+		lblAr.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblAr.setBounds(1017, 282, 120, 20);
+		contentPane.add(lblAr);
 		
-		lblNewLabel_10 = new JLabel("Frigobar");
-		lblNewLabel_10.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_10.setBounds(1017, 411, 120, 20);
-		contentPane.add(lblNewLabel_10);
+		lblFrigobar = new JLabel("Frigobar");
+		lblFrigobar.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblFrigobar.setBounds(1017, 411, 120, 20);
+		contentPane.add(lblFrigobar);
 		
-		lblNewLabel_11 = new JLabel("Banheira");
-		lblNewLabel_11.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_11.setBounds(1017, 548, 120, 20);
-		contentPane.add(lblNewLabel_11);
+		lblBanheira = new JLabel("Banheira");
+		lblBanheira.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblBanheira.setBounds(1017, 548, 120, 20);
+		contentPane.add(lblBanheira);
 		
-		lblNewLabel_12 = new JLabel("Acessibilidade");
-		lblNewLabel_12.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_12.setBounds(1017, 674, 120, 20);
-		contentPane.add(lblNewLabel_12);
+		lblAcessibilidade = new JLabel("Acessibilidade");
+		lblAcessibilidade.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblAcessibilidade.setBounds(1017, 674, 120, 20);
+		contentPane.add(lblAcessibilidade);
 		
-		lblNewLabel_13 = new JLabel("Limpeza");
-		lblNewLabel_13.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_13.setBounds(1506, 282, 120, 20);
-		contentPane.add(lblNewLabel_13);
+		lblLimpeza = new JLabel("Limpeza");
+		lblLimpeza.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblLimpeza.setBounds(1506, 282, 120, 20);
+		contentPane.add(lblLimpeza);
 		
-		lblNewLabel_14 = new JLabel("Conserto");
-		lblNewLabel_14.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_14.setBounds(1506, 411, 120, 20);
-		contentPane.add(lblNewLabel_14);
+		lblConserto = new JLabel("Conserto");
+		lblConserto.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblConserto.setBounds(1506, 411, 120, 20);
+		contentPane.add(lblConserto);
 		
-		lblNewLabel_15 = new JLabel("Preço");
-		lblNewLabel_15.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_15.setBounds(1506, 548, 120, 20);
-		contentPane.add(lblNewLabel_15);
+		lblPreco = new JLabel("Preço");
+		lblPreco.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblPreco.setBounds(1506, 548, 120, 20);
+		contentPane.add(lblPreco);
+		
+		JLabel lblFundoMenu = new JLabel("");
+		lblFundoMenu.setIcon(new ImageIcon("src/main/resources/fundo cinza (menu).png"));
+		lblFundoMenu.setBounds(0, 0, 420, 1080);
+		contentPane.add(lblFundoMenu);
+		
+		JLabel lblCaminho = new JLabel("");
+		lblCaminho.setIcon(new ImageIcon("src/main/resources/superior pequeno quartos.png"));
+		lblCaminho.setBounds(408, 0, 1512, 62);
+		contentPane.add(lblCaminho);
 	}
 }
