dfe923405a18cf2385dd9c1f6650587e02249cd7
unknown <gabriely.d@aluno.ifsc.edu.br>
2024-07-01 11:49:50 -0300

    Dao Hospedagem

--- Sigh/src/main/java/controle/hospedagem/IHospedagemDAO.java
+++ Sigh/src/main/java/controle/hospedagem/IHospedagemDAO.java
@@ -3,10 +3,14 @@ package controle.hospedagem;
 import java.util.ArrayList;
 
 import modelo.Hospedagem;
+import modelo.Hospede;
 
 public interface IHospedagemDAO {
 
 	public int inserirHospedagem(Hospedagem hosp);
+	
+	public int inserirHospedeHospedagem(Hospede hos, Hospedagem hosp);
+
 
 	public ArrayList<Hospedagem> listarHospedagem();
 
