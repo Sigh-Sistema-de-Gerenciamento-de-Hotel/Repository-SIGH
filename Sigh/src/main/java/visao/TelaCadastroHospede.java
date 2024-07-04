a5db9921fb22650799aab7b53268e70e0fb1869b
Lara <lara.p2006@aluno.ifsc.edu.br>
2024-07-04 09:03:40 -0300

    mask telefone

--- Sigh/src/main/java/visao/TelaCadastroHospede.java
+++ Sigh/src/main/java/visao/TelaCadastroHospede.java
@@ -14,6 +14,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.MaskFormatter;
 
 import controle.endereco.EnderecoDAO;
 import controle.hospede.HospedeDAO;
